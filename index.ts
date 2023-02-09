3b8bb050adcefef7e4da0580643b4b4bb66b08f1
Vitor Santos <githubvitor@gmail.com>
2023-02-09 18:12:59 -0300

    Alter message at file Index

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import * as express from "express";
+import * express from "express";
 
 const app = express();
 app.use(express.json());
@@ -7,8 +7,8 @@ app.get("/", (req, res) => {
   return res.send("Ok");
 });
 app.get("/teste", (req, res) => {
-  return res.send().json({
-    message: "Hellou Word!",
-  });
+  return res.status(200).json({
+    message:"Alteração no codigo de Teste"
+  })
 });
 app.listen(3030, () => console.log("Server is init..."));
