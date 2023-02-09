51f0dd4f9100dbe2726eb69dc229a6b6968da4be
Vitor Santos <githubvitor@gmail.com>
2023-02-09 17:03:57 -0300

    first commit

--- index.ts
+++ index.ts
@@ -7,6 +7,8 @@ app.get("/", (req, res) => {
   return res.send("Ok");
 });
 app.get("/teste", (req, res) => {
-  return res.send().json();
+  return res.send().json({
+    message: "Hellou Word!",
+  });
 });
 app.listen(3030, () => console.log("Server is init..."));
