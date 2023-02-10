a33c560e5905265591e2f54360743f72dab7e429
Vitor Santos <githubvitor@gmail.com>
2023-02-09 21:43:23 -0300

    new teste

--- index.ts
+++ index.ts
@@ -11,4 +11,9 @@ app.get("/teste", (req, res) => {
     message: "Alteração no codigo de Teste",
   });
 });
+app.get("/newcontent", (req, res) => {
+  return res.status(200).json({
+    message: "Testanto auto Pull",
+  });
+});
 app.listen(3030, () => console.log("Server is init..."));
