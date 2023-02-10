fa20d2757b72eed2e1f22beaa7ac8fe87e6c364c
Vitor Santos <githubvitor@gmail.com>
2023-02-09 21:49:55 -0300

    new update

--- index.js
+++ index.js
@@ -11,4 +11,9 @@ app.get("/teste", function (req, res) {
         message: "Alteração no codigo de Teste"
     });
 });
+app.get("/newcontent", function (req, res) {
+    return res.status(200).json({
+        message: "Testanto auto Pull"
+    });
+});
 app.listen(3030, function () { return console.log("Server is init..."); });
